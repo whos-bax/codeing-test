3d91fedba394be1af5b851fb1688ff94098b431c
whos-bax <loot04@naver.com>
2022-07-11 14:11:59 +0900

    first commit

---
+++ 1000.js
@@ -0,0 +1,6 @@
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+// let num = Number(input);
+let num = [1,2]
+console.log(num[0] + num[1])
---
+++ readme.md
@@ -0,0 +1,2 @@
+# 백준 문제 JavaScript 테스트 하기
+### 1. node.js 사용하기
