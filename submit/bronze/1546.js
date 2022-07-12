1dce14cc416768c1d7650e4559d5b1c77e4dccb2
whos-bax <loot04@naver.com>
2022-07-12 11:02:15 +0900

    1546

---
+++ submit/bronze/1546.js
@@ -0,0 +1,20 @@
+// 브론즈 1
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+const scores = input[1].split(" ");
+const newScores = [];
+const max = Math.max(...scores);
+for (const score of scores) {
+  newScores.push((score / max) * 100);
+}
+
+let sum = 0;
+for (const newScore of newScores) {
+  sum += newScore;
+}
+console.log(sum / n);
