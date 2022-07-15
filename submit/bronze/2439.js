a2fdc8db5762e872576487308bd8d41402701901
whos-bax <loot04@naver.com>
2022-07-15 12:41:16 +0900

    2439

--- submit/bronze/2439.js
+++ submit/bronze/2439.js
@@ -1,2 +1,11 @@
-// 브론즈 2
+// 브론즈 4
 let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input);
+
+for (let i = 1; i <= n; i++) {
+  i !== n ?
+  console.log(" ".repeat(n - i -1), "*".repeat(i))
+  :
+  console.log("*".repeat(i))
+}
