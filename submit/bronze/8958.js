fee15a401ad25b450ce187664dcc46daeb574661
whos-bax <loot04@naver.com>
2022-07-15 15:56:45 +0900

    8958

--- submit/bronze/8958.js
+++ submit/bronze/8958.js
@@ -1,6 +1,17 @@
 // 브론즈 2
-let input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split(" ");
+let input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+const strAsc = Array(8)
+  .fill()
+  .map((a, b) => b + 1)
+  .join(" ");
+
+const strDsc = Array(8)
+  .fill()
+  .map((a, b) => b + 1)
+  .sort((a, b) => b - a)
+  .join(" ");
+
+if (input === strAsc) console.log("ascending")
+else if (input === strDsc) console.log("descending")
+else console.log("mixed")
