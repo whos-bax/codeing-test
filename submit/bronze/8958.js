1e72a3ea8dc11d3562fd396894234cbe10a3c524
who <loot04@naver.com>
2022-07-18 13:14:46 +0900

    8958

--- submit/bronze/8958.js
+++ submit/bronze/8958.js
@@ -1,31 +1,4 @@
 // 브론즈 2
-<<<<<<< HEAD
-let input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-
-const [nums, ...quizzes] = input;
-
-for (const quiz of quizzes) {
-  let len = quiz.length;
-  let answer = 0;
-  let count = 0;
-  for (let i = 0; i < len; i++) {
-    if (quiz[i] === "O") {
-      count++;
-      if (quiz[i + 1] === "X" || quiz[i + 1] === undefined) {
-        // console.log(count)
-        for (let j = 0; j < count; j++) {
-          answer += j + 1;
-        }
-      }
-    } else count = 0;
-  }
-  console.log(answer);
-}
-=======
 let input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
 const strAsc = Array(8)
@@ -42,4 +15,3 @@ const strDsc = Array(8)
 if (input === strAsc) console.log("ascending")
 else if (input === strDsc) console.log("descending")
 else console.log("mixed")
->>>>>>> origin/formac
