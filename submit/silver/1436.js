a934821d7c3422ee09c92e255386ac98c42aa330
whos-bax <loot04@naver.com>
2022-08-10 15:49:18 +0900

    1436

--- submit/silver/1436.js
+++ submit/silver/1436.js
@@ -5,3 +5,16 @@ const input = require("fs")
   .trim()
   .split(/\s/);
 
+  let num = Number(input);
+
+  let first = 665;
+  
+  while (num > 0) {
+    first++;
+  
+    if (first.toString().includes("666")) {
+      num--;
+    }
+  }
+  
+  console.log(first);
