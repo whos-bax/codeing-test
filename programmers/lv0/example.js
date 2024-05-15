44f1bba09b1aa82aa6a2062886ffdc2020ea3ba2
whos-bax <bbak9604@gmail.com>
2024-05-15 15:05:25 +0900

    fix example2

--- programmers/lv0/example.js
+++ programmers/lv0/example.js
@@ -1,11 +1,12 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120885
 const example = require('./example/120885');
 
-function solution(input) {
+function solution({bin1, bin2}) {
     let answer = '0';
+    console.log(bin1, bin2)
     return answer;
 }
 
 for (let i = 0; i < example.result.length; i++) {
-    console.log(solution({...example.input}) === example.result[i]);
+    console.log(solution(example.input[i]) === example.result[i]);
 }
--- programmers/lv0/example/example.js
+++ programmers/lv0/example/example.js
@@ -1,9 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120885
 module.exports = example = {
-    input: {
-        bin1: ["10", "1001"],
-        bin2: ["11", "1111"],
-    },
+    input: [
+        {
+            bin1: ["10"],
+            bin2: ["11"],
+        },
+        {
+            bin1: ["1001"],
+            bin2: ["1111"],
+        }
+    ],
     result: [
         "101",
         "11000"
