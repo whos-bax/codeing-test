cd71f9352caa53a8319b6f73cd712dbdac045fac
whos-bax <bbak9604@gmail.com>
2024-05-22 10:00:06 +0900

    142086

---
+++ programmers/lv1/142086.js
@@ -0,0 +1,32 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/142086
+const example = require('./example/142086');
+
+function solution({s}) {
+    let answer = [];
+    // for (let i = 0; i < s.length; i++) {
+    //     let index = s.indexOf(s[i]);
+    //     if (index === i) {
+    //         index = -1;
+    //     } else {
+    //         for (let j = i - 1; j >= 0; j--) {
+    //             if (s[j] === s[i]) {
+    //                 index = i - j;
+    //                 break;
+    //             } else {
+    //                 index = i - index;
+    //             }
+    //         }
+    //     }
+    //     answer.push(index);
+    // }
+
+    answer = [...s].map((char, i) => {
+        const count = s.slice(0, i).lastIndexOf(char);
+        return count < 0 ? count : i - count;
+    });
+    return answer;
+}
+
+for (let i = 0; i < example.result.length; i++) {
+    console.log(solution(example.input[i]) === example.result[i]);
+}
---
+++ programmers/lv1/example/142086.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/142086
+module.exports = example = {
+    input: [
+        {
+            s: "banana"
+        },
+        {
+            s: "foobar"
+        },
+    ],
+    result: [
+        [-1, -1, -1, 2, 2, 2],
+        [-1, -1, 1, -1, -1, -1],
+    ]
+}
