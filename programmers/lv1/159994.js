c589cdb8198c82351a4ebae37df24a074f6e3481
whos-bax <bbak9604@gmail.com>
2024-06-01 12:04:34 +0900

    159994

---
+++ programmers/lv1/159994.js
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/159994
+const example = require('./example/159994');
+
+function solution({cards1, cards2, goal}) {
+    let answer = "Yes";
+    for (let i = 0; i < goal.length; i++) {
+        let word = goal[i];
+        if (cards1[0] === word) {
+            cards1.shift();
+        } else if (cards2[0] === word) {
+            cards2.shift();
+        } else {
+            answer = "No";
+            break;
+        }
+    }
+    return answer;
+    return answer;
+}
+
+for (let i = 0; i < example.result.length; i++) {
+    console.log(solution(example.input[i]) === example.result[i]);
+}
---
+++ programmers/lv1/example/159994.js
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/159994
+module.exports = example = {
+    input: [
+        {
+            cards1: ["i", "drink", "water"],
+            cards2: ["want", "to"],
+            goal: ["i", "want", "to", "drink", "water"]
+        },
+        {
+            cards1: ["i", "water", "drink"],
+            cards2: ["want", "to"],
+            goal: ["i", "want", "to", "drink", "water"]
+        },
+    ],
+    result: [
+        "Yes",
+        "No"
+    ]
+}
